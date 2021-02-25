8407284b7abf810a0290c94b06738591a9301bd4
hamidm317 <43106234+hamidm317@users.noreply.github.com>
2021-02-25 19:42:57 +0330

    Last Update

--- rf/trial.c
+++ rf/trial.c
@@ -1737,4 +1737,21 @@ int gotPhotodiodeAck(){
     return photodiodeAck;
 }
 
+void print_current_time_with_ms (void)
+{
+    long            ms; // Milliseconds
+    time_t          s;  // Seconds
+    struct timespec spec;
 
+    clock_gettime(CLOCK_REALTIME, &spec);
+
+    s  = spec.tv_sec;
+    ms = round(spec.tv_nsec / 1.0e6); // Convert nanoseconds to milliseconds
+    if (ms > 999) {
+        s++;
+        ms = 0;
+    }
+
+    printf("Current time: %"PRIdMAX".%03ld seconds since the Epoch\n",
+           (intmax_t)s, ms);
+}
